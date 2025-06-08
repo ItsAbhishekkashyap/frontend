94c35eaa9de5d9fd0679c2d1e2422d1e3069972d
ItsAbhishekkashyap <abhi47025@gmail.com>
2025-06-09 00:50:16 +0530

    shortId updated 1.2

--- app/[shortId]/route.ts
+++ app/[shortId]/route.ts
@@ -1,33 +1,4 @@
-// import { NextRequest, NextResponse } from 'next/server';
-// import { connectToDB } from '@/lib/mongodb';
-// import { Url } from '@/models/Url';
 
-// export async function GET(
-//   req: NextRequest,
-//   context: { params: { shortId: string } }
-// ) {
-//   try {
-//     await connectToDB();
-//     const { shortId } = context.params;
-
-//     const found = await Url.findOne({ slug: shortId });
-
-//     if (!found) {
-//       return NextResponse.redirect(new URL('/', req.url));
-//     }
-
-//     // Update analytics
-//     found.clicks = (found.clicks || 0) + 1;
-//     found.lastAccessed = new Date();
-//     found.clickHistory.push(new Date());
-//     await found.save();
-
-//     return NextResponse.redirect(found.originalUrl);
-//   } catch (error) {
-//     console.error('Redirect error:', error);
-//     return NextResponse.redirect(new URL('/', req.url));
-//   }
-// }
 
 
 
